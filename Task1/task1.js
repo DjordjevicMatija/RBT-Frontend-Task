3ebc81c555ff29faab2664541eceeed7201a6a7e
DjordjevicMatija <djordjevic.matija2@gmail.com>
2023-12-02 16:56:36 +0100

    task1 finished

--- Task1/task1.js
+++ Task1/task1.js
@@ -1,1 +1,55 @@
-console.log("Hello world");
+function validatePassword(password) {
+    if (password.length < 6) {
+        return "too short";
+    }
+    if (password.length > 24) {
+        return "too long";
+    }
+
+    let error = '';
+    let first = true;
+
+    if (!/[A-Z]/.test(password)) {
+        error += "missing upper case letter";
+        first = false;
+    }
+
+    if (!/[a-z]/.test(password)) {
+        if(!first){
+            error += ' and ';
+        }
+        error += "missing lower case letter";
+        first = false;
+    }
+
+    if (!/\d/.test(password)) {
+        if(!first){
+            error += ' and ';
+        }
+        error += "missing diggit";
+        first = false;
+    }
+
+    if (/(\w)\1{2,}/.test(password)) {
+        if(!first){
+            error += ' and ';
+        }
+        error += "more than 2 repeated characters";
+        first = false;
+    }
+
+    if (!/.*[^A-Za-z0-9\s].*[^A-Za-z0-9\s].*/.test(password)) {
+        if(!first){
+            error += ' and ';
+        }
+        error += "less than 2 special character";
+    }
+
+    if(error == '')
+        return "Password is valid";
+    else
+        return error;
+}
+console.log(validatePassword("P1zz@"));
+console.log(validatePassword("iLoveProgramming"));
+console.log(validatePassword("Fi#g91@"));
